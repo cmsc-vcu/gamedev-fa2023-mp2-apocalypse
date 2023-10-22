3cab0c86df1778e44acaa3f9048e3cc43b7a4e9a
Krainockc <krainockc@vcu.edu>
2023-10-21 19:37:01 -0700

    Vertical Animations applied, scene change functionality added

--- gamedev-fa2023-mp2-apocalypse/Assets/Scripts/PlayerControl.cs
+++ gamedev-fa2023-mp2-apocalypse/Assets/Scripts/PlayerControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerControl : MonoBehaviour
 {
@@ -8,31 +9,60 @@ public class PlayerControl : MonoBehaviour
     public Rigidbody2D rb;
     public float speed = 5;
     public Animator anim;
+    public Collider2D exit;
+    public GameObject mainCam;
+
+    Scene scene;
 
     Vector2 movement;
     // Start is called before the first frame update
     void Start()
     {
+        
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        exit = GameObject.FindWithTag("Exit").GetComponent<Collider2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        scene = SceneManager.GetActiveScene();
+
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
 
         
         anim.SetFloat("Horizontal",movement.x);
-        //anim.SetFloat("Vertical", movement.y);
+        anim.SetFloat("Vertical", movement.y);
         anim.SetFloat("Speed", movement.sqrMagnitude);
+        
+        //checks if player is in open world scene and makes camera follow player if so
+       
+        if (scene.name == "World")
+        {
+            mainCam.transform.position = transform.position + new Vector3(0, 0, -10f);
+        }
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision = exit) 
+        {
+            if (scene.name == "StartingRoom")
+            {
+                SceneManager.LoadScene("World");
+            }else if (scene.name == "World")
+            {
+                SceneManager.LoadScene("StartingRoom");
+            }
+            
+        }
     }
 }
